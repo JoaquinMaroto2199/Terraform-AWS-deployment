2825e1eeb20d569bd926dc867d5c0b1c7330e3fe
JoaquinMaroto2199 <joaquinmarpas@gmail.com>
2023-03-18 16:07:07 +0100

    Delete provider tem/ keys

--- .history/section2/provider_20230318103012.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region = "eu-west-1"
-  access_key = "AKIA5SGIGQ4G6WEWKAOD"
-  secret_key = "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"
-}
--- .history/section2/provider_20230318155105.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  region = "eu-west-1"
-  access_key = "AKIA5SGIGQ4GVJOZIN62"
-  secret_key = "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"
-}
