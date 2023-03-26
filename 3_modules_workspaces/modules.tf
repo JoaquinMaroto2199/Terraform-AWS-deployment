40f5569cc2f1205d40e1f3c438709920931704e2
JoaquinMaroto2199 <joaquinmarpas@gmail.com>
2023-03-26 18:33:46 +0200

    Changes .gitignore

--- 3_modules_workspaces/modules.tf
+++ 3_modules_workspaces/modules.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "test_ec2deployment" {
   ami = data.aws_ami.latest.id
-  instance_type = var.environment != "prod" ? var.ec2_instance_type_t2micro : var.ec2_instance_type_t2large
+  instance_type = lookup(var.ec2_instance_type, terraform.workspace)
   vpc_security_group_ids = [aws_security_group.ec2_secgroup.id]
   tags = {
     name = "Initial EC2"
@@ -11,16 +11,16 @@ resource "aws_security_group" "ec2_secgroup" {
     name = "ec2_secgroup"
 
     ingress {
-        from_port = 80
-        to_port = 80
+        from_port = 433
+        to_port = 433
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
 
     egress {
-        from_port = 80
-        to_port = 80
+        from_port = 433
+        to_port = 433
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
