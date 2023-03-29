c7075067fd7470dab5da46f4b81fdc387cd9d777
JoaquinMaroto2199 <joaquinmarpas@gmail.com>
2023-03-29 12:30:28 +0200

    Changes gitignore file

--- 4_remote_management/data.tf
+++ 4_remote_management/data.tf
@@ -7,3 +7,12 @@ data "aws_ami" "latest" {
     values = ["amzn2-ami-ecs-hvm-2.0.202*-x86_64-ebs"]
   }
 }
+
+data "terraform_remote_state" "eip" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-joaquin-bucket"
+        key = "network/eip.tfstate"
+        region = "eu-west-1"
+  }
+}
